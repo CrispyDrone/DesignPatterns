535d566c31e01755220e3008c2a47bd806a0240e
CrispyDrone <mybluewaterfall@hotmail.com>
2019-11-29 18:58:57 +0100

    feat: Add singleton

---
+++ OOP.DesignPatterns/GoF/Creational/Singleton/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace OOP.DesignPatterns.GoF.Creational.Singleton
+{
+    // not threadsafe
+    public class Logger
+    {
+        private string _logPath;
+
+        private Logger(string logPath)
+        {
+            _logPath = logPath;
+        }
+
+        public static Logger Instance { get; private set; }
+
+        public static void Initialize(string logPath)
+        {
+            if (Instance == null)
+                Instance = new Logger(logPath);
+        }
+
+        public void Log(string message)
+        {
+            using (var fs = new FileStream(_logPath, FileMode.Append))
+            using (var sw = new StreamWriter(fs))
+            {
+                sw.WriteLine($"{DateTime.Now} - {message}");
+            }
+        }
+    }
+}
